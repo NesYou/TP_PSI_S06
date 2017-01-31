777053a8c00c7ea424cf6270e23b56f800bebf6b
NesYou <younes.bouayadine@gmail.com>
2017-01-31 23:41:52 +0100

    Finalisation de l'affichage des commentaires.

--- FilmsEtCommentaires/commentaire.php
+++ FilmsEtCommentaires/commentaire.php
@@ -7,8 +7,12 @@
   $commentaires = get_commentaire($connexion);
   require_once('includes/get_filmAll.php');
   $films = get_filmAll($connexion);
+  require_once('includes/get_acteurAll.php');
+  $acteurs = get_acteurAll($connexion);
 
   echo $twig->render('commentaire.html.twig', array(
     'GET' => $_GET,
     'commentaires' => $commentaires,
+    'films' => $films,
+    'acteurs' => $acteurs
   ));
