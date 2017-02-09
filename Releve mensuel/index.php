c35719b6e89e4b706408398111d5f594067bd54b
NesYou <younes.bouayadine@gmail.com>
2017-02-09 18:27:47 +0100

    Ajout des premiers fichiers nécessaires à l'initialisation.

---
+++ Releve mensuel/index.php
@@ -0,0 +1,20 @@
+<?php
+  require_once ('InstancierTwig.php');
+  require_once('includes/dbconnect.php');
+  $connexion = connect();
+
+  function chargerClasse($classe) {
+    require_once dirname(__FILE__) . '/includes/' . $classe . '.class.php';
+  }
+  spl_autoload_register('chargerClasse');
+
+  $manager = new BlogManager($connexion);
+  $films = $manager->getListFilm();
+  $acteurs = $manager->getActeur();
+  
+  
+
+  echo $twig->render('index.html.twig', array(
+    'films' => $films,
+    'acteurs' => $acteurs
+  ));
